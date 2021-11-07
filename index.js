6b2589645d7b190db05663e24fb2c8a6f03063d5
Andrew Bwogi <Andrew.bwogi@gmail.com>
2021-11-07 02:15:50 -0800

    first test route

--- index.js
+++ index.js
@@ -10,8 +10,17 @@ dotenv.config(); // its ready to use to hide secret keys
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
     console.log("Backend Server connected to MongoDB Atlas...")
+});
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+//routes
+app.get('/', (req, res) => {
+    res.send("Welcome to homepage")
 })
 
 const PORT = process.env.PORT || 8800; // the port to connect to
-
 app.listen(PORT, () => console.log(`Backend server connected on http://localhost:${PORT}`))
